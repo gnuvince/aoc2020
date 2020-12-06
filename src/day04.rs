848cf9bd4d859996a733dc51e7c8a2ba20faf783
Vincent Foley <vfoley@gmail.com>
2020-12-06 18:00:28 -0500

    Refactor Day 04 - use read_groups_of_lines()

--- src/day04.rs
+++ src/day04.rs
@@ -1,4 +1,4 @@
-use std::io::{self, BufRead};
+use std::io;
 
 #[derive(Default)]
 struct PassportValidation {
@@ -17,95 +17,88 @@ impl PassportValidation {
     }
 }
 
-fn main() -> anyhow::Result<()> {
+fn part1(g: &Vec<Vec<String>>) -> usize {
+    let mut valid: usize = 0;
+    for group in g {
+        let mut pv = PassportValidation::default();
+        for line in group {
+            pv.byr = pv.byr || line.contains("byr");
+            pv.iyr = pv.iyr || line.contains("iyr");
+            pv.eyr = pv.eyr || line.contains("eyr");
+            pv.hgt = pv.hgt || line.contains("hgt");
+            pv.hcl = pv.hcl || line.contains("hcl");
+            pv.ecl = pv.ecl || line.contains("ecl");
+            pv.pid = pv.pid || line.contains("pid");
+        }
+        valid += pv.is_valid() as usize;
+    }
+    return valid;
+}
+
+fn part2(g: &Vec<Vec<String>>) -> anyhow::Result<usize> {
     let hgt_re = regex::Regex::new(r"^(\d+)(cm|in)$")?;
     let hcl_re = regex::Regex::new(r"^#[0-9a-f]{6}$")?;
     let ecl_re = regex::Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$")?;
     let pid_re = regex::Regex::new(r"^[0-9]{9}$")?;
 
-    let stdin = io::stdin();
-    let stdin = stdin.lock();
-
-    let mut p1_valid: usize = 0;
-    let mut p1_curr = PassportValidation::default();
-    let mut p2_valid: usize = 0;
-    let mut p2_curr = PassportValidation::default();
-
-    for line in stdin.lines() {
-        let line = line?;
-        if line.is_empty() {
-            if p1_curr.is_valid() {
-                p1_valid += 1;
-            }
-            p1_curr = PassportValidation::default();
-
-            if p2_curr.is_valid() {
-                p2_valid += 1;
-            }
-            p2_curr = PassportValidation::default();
-        } else {
-            for group in line.split(' ') {
-                if group.starts_with("byr") {
-                    p1_curr.byr = true;
-                    if let Ok(yr) = (&group[4..]).parse::<i32>() {
-                        p2_curr.byr = yr >= 1920 && yr <= 2002;
+    let mut valid: usize = 0;
+    for group in g {
+        let mut pv = PassportValidation::default();
+        for line in group {
+            for chunk in line.split(' ') {
+                if chunk.starts_with("byr") {
+                    if let Ok(yr) = (&chunk[4..]).parse::<i32>() {
+                        pv.byr = yr >= 1920 && yr <= 2002;
                     }
                 }
 
-                if group.starts_with("iyr") {
-                    p1_curr.iyr = true;
-                    if let Ok(yr) = (&group[4..]).parse::<i32>() {
-                        p2_curr.iyr = yr >= 2010 && yr <= 2020;
+                if chunk.starts_with("iyr") {
+                    if let Ok(yr) = (&chunk[4..]).parse::<i32>() {
+                        pv.iyr = yr >= 2010 && yr <= 2020;
                     }
                 }
 
-                if group.starts_with("eyr") {
-                    p1_curr.eyr = true;
-                    if let Ok(yr) = (&group[4..]).parse::<i32>() {
-                        p2_curr.eyr = yr >= 2020 && yr <= 2030;
+                if chunk.starts_with("eyr") {
+                    if let Ok(yr) = (&chunk[4..]).parse::<i32>() {
+                        pv.eyr = yr >= 2020 && yr <= 2030;
                     }
                 }
 
-                if group.starts_with("hgt") {
-                    p1_curr.hgt = true;
-                    if let Some(caps) = hgt_re.captures(&group[4..]) {
+                if chunk.starts_with("hgt") {
+                    if let Some(caps) = hgt_re.captures(&chunk[4..]) {
                         let hgt: i32 = caps.get(1).unwrap().as_str().parse::<i32>()?;
                         let units = caps.get(2).unwrap().as_str();
                         if units == "in" {
-                            p2_curr.hgt = hgt >= 59 && hgt <= 76;
+                            pv.hgt = hgt >= 59 && hgt <= 76;
                         } else if units == "cm" {
-                            p2_curr.hgt = hgt >= 150 && hgt <= 193;
+                            pv.hgt = hgt >= 150 && hgt <= 193;
                         }
                     }
                 }
 
-                if group.starts_with("hcl") {
-                    p1_curr.hcl = true;
-                    p2_curr.hcl = hcl_re.is_match(&group[4..]);
+                if chunk.starts_with("hcl") {
+                    pv.hcl = hcl_re.is_match(&chunk[4..]);
                 }
 
-                if group.starts_with("ecl") {
-                    p1_curr.ecl = true;
-                    p2_curr.ecl = ecl_re.is_match(&group[4..]);
+                if chunk.starts_with("ecl") {
+                    pv.ecl = ecl_re.is_match(&chunk[4..]);
                 }
 
-                if group.starts_with("pid") {
-                    p1_curr.pid = true;
-                    p2_curr.pid = pid_re.is_match(&group[4..]);
+                if chunk.starts_with("pid") {
+                    pv.pid = pid_re.is_match(&chunk[4..]);
                 }
             }
         }
+        valid += pv.is_valid() as usize;
     }
+    return Ok(valid);
+}
 
-    if p1_curr.is_valid() {
-        p1_valid += 1;
-    }
-    if p2_curr.is_valid() {
-        p2_valid += 1;
-    }
-
-    println!("part 1: {}", p1_valid);
-    println!("part 2: {}", p2_valid);
-
+fn main() -> anyhow::Result<()> {
+    let stdin = io::stdin();
+    let stdin = stdin.lock();
+    let groups = aoc2020::read_groups_of_lines(stdin)?;
+    println!("part 1: {}", part1(&groups));
+    println!("part 2: {}", part2(&groups)?);
     return Ok(());
 }
