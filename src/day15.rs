85cb6429eb1c4f2bee10d25513154e87cf316308
Vincent Foley <vfoley@gmail.com>
2020-12-17 20:51:05 -0500

    Use a vec instead of a map in day 15

--- src/day15.rs
+++ src/day15.rs
@@ -1,34 +1,35 @@
-use std::collections::HashMap;
 use std::io;
 
-fn parts(turns: usize, nums: &Vec<usize>) -> usize {
-    let mut cache: HashMap<usize, usize> = HashMap::new();
+fn parts(turns: usize, nums: &Vec<i32>) -> usize {
+    let mut cache: Vec<i32> = vec![-1; turns];
     for i in 0..nums.len() - 1 {
-        cache.insert(nums[i], i + 1);
+        cache[nums[i] as usize] = (i+1) as i32;
     }
-    let mut prev: usize = nums[nums.len() - 1];
+
+    let mut curr: usize = nums[nums.len() - 1] as usize;
 
     for t in nums.len()..turns {
-        if let Some(x) = cache.get(&prev) {
-            let p = prev;
-            prev = t - x;
-            cache.insert(p, t);
-        } else {
-            cache.insert(prev, t);
-            prev = 0;
+        match cache[curr] {
+            -1 => {
+                cache[curr] = t as i32;
+                curr = 0;
+            }
+            x => {
+                cache[curr] = t as i32;
+                curr = (t as i32 - x) as usize;
+            }
         }
     }
-
-    return prev;
+    return curr;
 }
 
 fn main() -> anyhow::Result<()> {
     let stdin = io::stdin();
     let stdin = stdin.lock();
     let lines = aoc2020::read_lines_string(stdin)?;
-    let mut nums: Vec<usize> = vec![];
+    let mut nums: Vec<i32> = vec![];
     for s in lines[0].split(',') {
-        nums.push(s.parse::<usize>()?);
+        nums.push(s.parse::<i32>()?);
     }
 
     println!("part 1: {}", parts(2020, &nums));
